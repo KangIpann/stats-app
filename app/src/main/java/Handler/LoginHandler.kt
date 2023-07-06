92e384fc7b922ac98977edd87fc64208165b58fa
fahrimaulanaa <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2023-07-06 17:08:36 +0700

    Add login mechanism to home

--- app/src/main/java/Handler/LoginHandler.kt
+++
@@ -1,4 +0,0 @@
-package Handler
-
-class LoginHandler {
-}
--- app/src/main/java/com/example/statsapp/starter/Login.kt
+++ app/src/main/java/com/example/statsapp/starter/Login.kt
@@ -5,18 +5,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.statsapp.R
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity(), OnClickListener {
     private lateinit var tvSignUp: TextView
+    private lateinit var et_email: EditText
+    private lateinit var et_password: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var pb_login: ProgressBar
+
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         window.statusBarColor = ContextCompat.getColor(this, com.google.android.material.R.color.mtrl_btn_transparent_bg_color)
         tvSignUp = findViewById(R.id.tvSignUp)
+        btnLogin = findViewById(R.id.btn_login)
+        pb_login = findViewById(R.id.pb_login)
+        et_email = findViewById(R.id.emailEt)
+        et_password = findViewById(R.id.passEt)
         tvSignUp.setOnClickListener(this)
+        btnLogin.setOnClickListener(this)
+
+        auth = FirebaseAuth.getInstance()
+        pb_login.visibility = View.GONE
 
     }
 
@@ -25,6 +44,28 @@ class Login : AppCompatActivity(), OnClickListener {
             R.id.tvSignUp -> {
                 val intent = Intent(this, SignUp::class.java)
                 startActivity(intent)
+            }
+            R.id.btn_login -> {
+                pb_login.visibility = View.VISIBLE
+                val email = et_email.text.toString()
+                val password = et_password.text.toString()
+                if (email.isNotEmpty() && password.isNotEmpty()) {
+                    auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                pb_login.visibility = View.GONE
+                                val intent = Intent(this, Home::class.java)
+                                startActivity(intent)
+                            } else {
+                                pb_login.visibility = View.GONE
+                                // Login failed
+                                Toast.makeText(this, "Email atau Password salah", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }else{
+                    pb_login.visibility = View.GONE
+                    Toast.makeText(this, "Email dan Password tidak boleh kosong", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
