9f3899492e1a78c425db35d144c3c8f7bb739b23
fahrimaulanaa <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2023-07-17 19:36:49 +0700

    rispek risih pepek

---
+++ app/src/main/java/Adapter/PemainListAdapter.kt
@@ -0,0 +1,65 @@
+import Data.pemainData
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.statsapp.R
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+
+class PemainListAdapter(private val query: Query) :
+    RecyclerView.Adapter<PemainListAdapter.PemainViewHolder>() {
+
+    private var pemains : MutableList<pemainData> = mutableListOf()
+
+    inner class PemainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val namaPemainTextView: TextView = itemView.findViewById(R.id.nama_pemain)
+        private val rolePemainTextView: TextView = itemView.findViewById(R.id.role_pemain)
+        private val nomorPemainTextView: TextView = itemView.findViewById(R.id.no_punggung)
+
+        fun bind(pemain: pemainData) {
+            namaPemainTextView.text = pemain.nama_pemain
+            rolePemainTextView.text = pemain.role_pemain
+            nomorPemainTextView.text = pemain.no_punggung
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PemainViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView = inflater.inflate(R.layout.pemain_row, parent, false)
+        return PemainViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return pemains.size
+    }
+
+    override fun onBindViewHolder(holder: PemainViewHolder, position: Int) {
+        val pemain = pemains[position]
+        holder.bind(pemain)
+    }
+
+    fun startListening() {
+        query.addSnapshotListener { snapshot: QuerySnapshot?, exception: FirebaseFirestoreException? ->
+            if (exception != null) {
+                return@addSnapshotListener
+            }
+
+            pemains.clear() // Clear the list before adding new data
+
+            for (document in snapshot?.documents!!) {
+                val pemain = document.toObject(pemainData::class.java)
+                pemain?.id = document.id
+                pemains.add(pemain!!)
+            }
+
+            notifyDataSetChanged()
+        }
+    }
+
+}
--- app/src/main/java/Tim/PemainList.kt
+++ app/src/main/java/Tim/PemainList.kt
@@ -1,12 +1,35 @@
 package Tim
 
+import PemainListAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.statsapp.R
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class PemainList : AppCompatActivity() {
+
+    private lateinit var adapter: PemainListAdapter
+    private lateinit var db: FirebaseFirestore
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var documentId: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pemain_list)
+
+        documentId = intent.getStringExtra("documentId").toString()
+        println("documentId yang diterima: $documentId")
+        val recyclerView = findViewById<RecyclerView>(R.id.pemain_rv_teams)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        val db = FirebaseFirestore.getInstance()
+        //cari pada collection pemain, yang memiliki field id_tim_pemain yang sama dengan documentId
+        val query = db.collection("pemain").whereEqualTo("id_tim_pemain", documentId)
+        val adapter = PemainListAdapter(query)
+        recyclerView.adapter = adapter
+        adapter.startListening()
     }
 }
