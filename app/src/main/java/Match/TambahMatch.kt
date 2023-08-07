3b06e4e3a8051798eb62619fe2dd0c04ecdf6654
fahrimaulanaa <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2023-08-07 11:37:53 +0700

    Tambah Match Finished

--- app/src/main/java/Match/TambahMatch.kt
+++ app/src/main/java/Match/TambahMatch.kt
@@ -3,6 +3,7 @@ package Match
 import Adapter.CustomSpinnerAdapter
 import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -23,6 +24,10 @@ class TambahMatch : AppCompatActivity() {
     private lateinit var db: FirebaseFirestore
     private lateinit var etNamaMatch: EditText
     private lateinit var tvTanggalMatch: TextView
+    private lateinit var spinnerTeamHome: Spinner
+    private lateinit var spinnerTeamAway: Spinner
+    private lateinit var spinnerJerseyHome: Spinner
+    private lateinit var spinnerJerseyAway: Spinner
     private lateinit var etStartTime: TextInputEditText
     private lateinit var etEndTime: TextInputEditText
     private lateinit var etSeason: EditText
@@ -41,7 +46,7 @@ class TambahMatch : AppCompatActivity() {
         etCompetitions = findViewById(R.id.et_competition_matchmaking)
         LinearStartMatch = findViewById(R.id.linear_startMatch)
 
-        // handler ketika tombol tanggal di klik
+        // handler ketika tombol tanggal N klik
         tvTanggalMatch.setOnClickListener {
             showDatePickerDialog()
         }
@@ -51,109 +56,104 @@ class TambahMatch : AppCompatActivity() {
         val spinnerJerseyHome = findViewById<Spinner>(R.id.spinner_jersey_tim_home)
         val spinnerJerseyAway = findViewById<Spinner>(R.id.spinner_jersey_tim_away)
 
+        //inisiaisasi spinner team
         val teamNamesList = ArrayList<String>()
         teamNamesList.add("Pilih Tim") // Add the default value
         val teamAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, teamNamesList)
         teamAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
         spinnerTeamHome.adapter = teamAdapter
         spinnerTeamAway.adapter = teamAdapter
 
-        val jerseyAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, mutableListOf<String>())
+        //inisialisasi spinner jersey
+        val jerseyList = ArrayList<String>()
+        jerseyList.add("Pilih Jersey") // Add the default value
+        val jerseyAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, jerseyList)
         jerseyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
         spinnerJerseyHome.adapter = jerseyAdapter
         spinnerJerseyAway.adapter = jerseyAdapter
 
-        // Initialize Firestore
         db = FirebaseFirestore.getInstance()
+        val teamRef = db.collection("team")
 
-        // Fetch data from Firestore and populate the teamNamesList
-        val teamCollectionRef = db.collection("team")
-
-        teamCollectionRef.get()
+        //mengambil data team dari firestore
+        teamRef.get()
             .addOnSuccessListener { documents ->
                 for (document in documents) {
-                    val teamName = document.getString("nama_team")
-                    if (teamName != null) {
-                        teamNamesList.add(teamName)
-                    }
+                    val teamName = document.data["nama_team"].toString()
+                    val jerseyColor = document.data["jersey"].toString()
+                    println("teamName yang dipilih: $teamName dan jersey: $jerseyColor")
+                    teamNamesList.add(teamName)
+                    jerseyList.add(jerseyColor)
                 }
                 teamAdapter.notifyDataSetChanged()
+                jerseyAdapter.notifyDataSetChanged()
             }
-            .addOnFailureListener { exception ->
-                // Handle the error
+            .addOnFailureListener() { exception ->
+                println("Error getting documents: $exception")
             }
 
-        // Set up the listener for team selection
+        //handler ketika spinner team home dipilih
         spinnerTeamHome.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedTeam = parent?.getItemAtPosition(position).toString()
-                // Fetch and populate jersey data for the selected team
-                fetchJerseyData(selectedTeam, spinnerJerseyHome)
-                fetchDocumentId(selectedTeam, "home")
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("Nothing Selected")
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Handle when nothing is selected
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val teamName = parent?.getItemAtPosition(position).toString()
+                println("teamName yang dipilih: $teamName")
+                if (teamName != "Pilih Tim") {
+                    val teamRef = db.collection("team").whereEqualTo("nama_team", teamName)
+                    teamRef.get()
+                        .addOnSuccessListener { documents ->
+                            for (document in documents) {
+                                val jerseyColor = document.data["jersey"].toString()
+                                println("jerseyColor yang dipilih: $jerseyColor")
+                                spinnerJerseyHome.setSelection(jerseyAdapter.getPosition(jerseyColor))
+                            }
+                        }
+                        .addOnFailureListener() { exception ->
+                            println("Error getting documents: $exception")
+                        }
+                }
             }
         }
 
-        // Similar listener for spinnerTeamAway
+        //handler ketika spinner team away dipilih
         spinnerTeamAway.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedTeam = parent?.getItemAtPosition(position).toString()
-                // Fetch and populate jersey data for the selected team
-                fetchJerseyData(selectedTeam, spinnerJerseyAway)
-                fetchDocumentId(selectedTeam, "away")
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("Nothing Selected")
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Handle when nothing is selected
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val teamName = parent?.getItemAtPosition(position).toString()
+                println("teamName yang dipilih: $teamName")
+                if (teamName != "Pilih Tim") {
+                    val teamRef = db.collection("team").whereEqualTo("nama_team", teamName)
+                    teamRef.get()
+                        .addOnSuccessListener { documents ->
+                            for (document in documents) {
+                                val jerseyColor = document.data["jersey"].toString()
+                                println("jerseyColor yang dipilih: $jerseyColor")
+                                spinnerJerseyAway.setSelection(jerseyAdapter.getPosition(jerseyColor))
+                            }
+                        }
+                        .addOnFailureListener() { exception ->
+                            println("Error getting documents: $exception")
+                        }
+                }
             }
         }
-    }
 
-    private fun fetchJerseyData(selectedTeam: String, jerseySpinner: Spinner) {
-        val jerseyCollectionRef = db.collection("team")
-            .whereEqualTo("nama_team", selectedTeam)
+        //handler untuk waktu mulai dan selesai
+        etStartTime.setOnClickListener {
+            setStartTime()
+        }
 
-        jerseyCollectionRef.get()
-            .addOnSuccessListener { documents ->
-                if (!documents.isEmpty) {
-                    val jerseyData = documents.documents[0].getString("jersey")
-                    if (jerseyData != null) {
-                        val jerseyList = jerseyData.split(",") // You may need to adjust this based on how your jersey data is stored
-                        val jerseyAdapter = jerseySpinner.adapter as ArrayAdapter<String>
-                        jerseyAdapter.clear()
-                        jerseyAdapter.addAll(jerseyList)
-                        jerseyAdapter.notifyDataSetChanged()
-                    }
-                }
-            }
-            .addOnFailureListener { exception ->
-                // Handle the error
-            }
-    }
+        etEndTime.setOnClickListener {
+            setEndTime()
+        }
 
-    private fun selectedTeamDocumentId(selectedTeam: String, homeOrAway: String) {
-        val teamCollectionRef = db.collection("team")
-            .whereEqualTo("nama_team", selectedTeam)
-
-        teamCollectionRef.get()
-            .addOnSuccessListener { documents ->
-                if (!documents.isEmpty) {
-                    val documentId = documents.documents[0].id
-                    if (homeOrAway == "home") {
-                        teamData.id = documentId
-                    } else {
-                        teamData.id = documentId
-                    }
-                }
-            }
-            .addOnFailureListener { exception ->
-                // Handle the error
-            }
+        //mengambil data yang diperlukan untuk lanjut ke activity match
     }
 
     private fun showDatePickerDialog() {
@@ -175,5 +175,106 @@ class TambahMatch : AppCompatActivity() {
         )
         datePickerDialog.show()
         datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
+    }
+
+    private fun setStartTime() {
+        val calendar = Calendar.getInstance()
+        val timeSetListener =
+            TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+                val startTimeEditText = findViewById<TextView>(R.id.et_start_time)
+                calendar.set(Calendar.HOUR_OF_DAY, hour)
+                calendar.set(Calendar.MINUTE, minute)
+                startTimeEditText.text =
+                    hour.toString() + ":" + minute + ":00" + "     -"
+            }
+        val timePickerDialog = TimePickerDialog(
+            this, timeSetListener,
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            true
+        )
+        timePickerDialog.show()
+    }
+
+    private fun setEndTime() {
+        val calendar = Calendar.getInstance()
+        val timeSetListener =
+            TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+                val endTimeEditText = findViewById<TextView>(R.id.et_end_time)
+                calendar.set(Calendar.HOUR_OF_DAY, hour)
+                calendar.set(Calendar.MINUTE, minute)
+                endTimeEditText.text =
+                    hour.toString() + ":" + minute + ":00"
+            }
+        val timePickerDialog = TimePickerDialog(
+            this, timeSetListener,
+            calendar.get(Calendar.HOUR_OF_DAY),
+            calendar.get(Calendar.MINUTE),
+            true
+        )
+        timePickerDialog.show()
+    }
+    private fun getHomeTeamPlayers() {
+        val teamName = spinnerTeamHome.selectedItem.toString()
+
+        db.collection("team")
+            .whereEqualTo("nama_team", teamName)
+            .get()
+            .addOnSuccessListener { teamDocuments ->
+                for (teamDocument in teamDocuments) {
+                    val teamId = teamDocument.id
+
+                    db.collection("pemain")
+                        .whereEqualTo("id_tim_pemain", teamId)
+                        .get()
+                        .addOnSuccessListener { playerDocuments ->
+                            for (playerDocument in playerDocuments) {
+                                val playerName = playerDocument.getString("nama_pemain")
+                                val playerNumber = playerDocument.getString("no_punggung")
+
+                                // Do something with playerName and playerNumber
+                                println("Player Name: $playerName, Player Number: $playerNumber")
+                            }
+                        }
+                        .addOnFailureListener { exception ->
+                            // Handle the error
+                        }
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Handle the error
+            }
+    }
+
+    private fun getAwayTeamPlayers() {
+        val teamName = spinnerTeamAway.selectedItem.toString()
+
+        db.collection("team")
+            .whereEqualTo("nama_team", teamName)
+            .get()
+            .addOnSuccessListener { teamDocuments ->
+                for (teamDocument in teamDocuments) {
+                    val teamId = teamDocument.id
+
+                    db.collection("pemain")
+                        .whereEqualTo("id_tim_pemain", teamId)
+                        .get()
+                        .addOnSuccessListener { playerDocuments ->
+                            for (playerDocument in playerDocuments) {
+                                val playerName = playerDocument.getString("nama_pemain")
+                                val playerNumber = playerDocument.getString("no_punggung")
+
+                                // Do something with playerName and playerNumber
+                                println("Player Name: $playerName, Player Number: $playerNumber")
+                            }
+                        }
+                        .addOnFailureListener { exception ->
+                            // Handle the error
+                        }
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Handle the error
+            }
     }
 }
